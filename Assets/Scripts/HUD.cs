af55911dcb80637961a35842750968f3ea30c27d
tarpaha <ppopchikovsky@gmail.com>
2018-08-17 18:58:39 +0300

    initial commit

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+	public Camera Camera;
+	public Transform ObjectToTrace;
+
+	public RectTransform CrosshairParentRect;
+	public RectTransform Crosshair;
+
+	private void LateUpdate()
+	{
+		Vector2 pos = Camera.WorldToViewportPoint(ObjectToTrace.position);
+		pos.x = CrosshairParentRect.sizeDelta.x * pos.x;
+		pos.y = CrosshairParentRect.sizeDelta.y * pos.y;
+		Crosshair.anchoredPosition = pos;
+	}
+}
---
+++ Assets/Scripts/RandomMovementController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomMovementController : MonoBehaviour
+{
+    public float Radius = 3.0f;
+    public float MoveDuration = 1.0f;
+
+    private void Start()
+    {
+        Random.InitState(DateTime.Now.Millisecond);
+        MoveToRandomPosition();
+    }    
+    
+    private void Update()
+    {
+        _moveTime += Time.deltaTime;
+        if (_moveTime < MoveDuration)
+        {            
+            transform.localPosition = Vector3.Lerp(_from, _to, Smooth(_moveTime / MoveDuration));            
+        }
+        else
+        {
+            transform.localPosition = _to;
+            MoveToRandomPosition();
+        }
+    }
+
+    private void MoveToRandomPosition()
+    {
+        _from = transform.localPosition;
+        _to = Radius * Random.onUnitSphere;
+        _moveTime = 0;
+    }
+
+    private static float Smooth(float t)
+    {
+        return 0.5f * (1.0f - Mathf.Cos(Mathf.PI * t));
+    }
+
+    private float _moveTime;
+    private Vector3 _from;
+    private Vector3 _to;
+}
